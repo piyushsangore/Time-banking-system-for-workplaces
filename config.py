609df766e3a9da720975b62a348816f27f1ffd1f
Piyush Sangore <piyushsanhore147@gmail.com>
2026-03-23 19:23:33 +0530

    remove Mongo URI for safety issues

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_secret_key')
     MONGO_URI = os.getenv(
         'MONGO_URI',
-        'mongodb+srv://admin:admin@gps.nzfxn7s.mongodb.net/?retryWrites=true&w=majority&appName=GPS',
+        'YOUR_MONGO_URI',
     )
